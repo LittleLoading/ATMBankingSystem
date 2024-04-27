64b00b16cf750d0c74acdbfb51d7056531e4b6e6
Hnba <dhourda@gmail.com>
2024-04-27 22:26:05 +0200

    form update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
-public class Main {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+    private JLabel jsFirstName;
+    private JTextField textFieldFirstName;
+    private JButton Value;
+    private JPanel MainPanel;
+    private JLabel jsLastName;
+    private JTextField textFieldLastName;
+
+    public Main(){
+        setContentPane(MainPanel);
+        setTitle("ATM Banking System");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        Value.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String firstname = textFieldFirstName.getText();
+                JOptionPane.showMessageDialog(Main.this, "Enter First Name:");
+            }
+        });
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        new Main();
+
+
+
     }
 }
